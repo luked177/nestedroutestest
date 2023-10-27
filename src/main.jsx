a7d7a0475a9e9fa14d5a493d2db715d13d306f7f
luked177 <43680876+luked177@users.noreply.github.com>
2023-10-27 19:07:30 +0100

    Revert

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { Navigate, RouterProvider, createBrowserRouter } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Path1 from "./Path1.jsx";
 import GuidChild from "./GuidChild.jsx";
 
 const router = createBrowserRouter(
 	[
-		{
-			path: "*",
-			element: <Navigate to={"/"} replace />,
-		},
 		{
 			path: "/",
 			element: <App />,
