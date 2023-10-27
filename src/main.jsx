e35bc7a9dfac6ae4458e5974fac1e52ac71c3acd
luked177 <43680876+luked177@users.noreply.github.com>
2023-10-27 19:33:54 +0100

    Link to development/portal

--- src/main.jsx
+++ src/main.jsx
@@ -6,26 +6,29 @@ import { Navigate, RouterProvider, createBrowserRouter } from "react-router-dom"
 import Path1 from "./Path1.jsx";
 import GuidChild from "./GuidChild.jsx";
 
-const router = createBrowserRouter([
-	{
-		path: "*",
-		element: <Navigate to={"/"} replace />,
-	},
-	{
-		path: "/",
-		element: <App />,
-		children: [
-			{
-				path: "/path1",
-				element: <Path1 />,
-			},
-			{
-				path: "/child/:guid",
-				element: <GuidChild />,
-			},
-		],
-	},
-]);
+const router = createBrowserRouter(
+	[
+		{
+			path: "*",
+			element: <Navigate to={"/"} replace />,
+		},
+		{
+			path: "/",
+			element: <App />,
+			children: [
+				{
+					path: "/path1",
+					element: <Path1 />,
+				},
+				{
+					path: "/child/:guid",
+					element: <GuidChild />,
+				},
+			],
+		},
+	],
+	{ basename: "/development/portal" }
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
