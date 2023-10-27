7d0f4964d0157fe7fc04e1b2b99d5db680ddd306
luked177 <43680876+luked177@users.noreply.github.com>
2023-10-27 19:20:05 +0100

    Update routes

--- src/main.jsx
+++ src/main.jsx
@@ -2,26 +2,35 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Navigate, RouterProvider, createBrowserRouter } from "react-router-dom";
 import Path1 from "./Path1.jsx";
 import GuidChild from "./GuidChild.jsx";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+	[
+		{
+			path: "*",
+			element: <Navigate to={"/"} replace />,
+		},
+		{
+			path: "/",
+			element: <App />,
+			children: [
+				{
+					path: "/path1",
+					element: <Path1 />,
+				},
+				{
+					path: "/child/:guid",
+					element: <GuidChild />,
+				},
+			],
+		},
+	],
 	{
-		path: "/nested",
-		element: <App />,
-		children: [
-			{
-				path: "/nested/path1",
-				element: <Path1 />,
-			},
-			{
-				path: "/nested/child/:guid",
-				element: <GuidChild />,
-			},
-		],
-	},
-]);
+		basename: "/nested",
+	}
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
