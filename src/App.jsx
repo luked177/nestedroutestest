c175457c1bfad2df2dbe0117b4aae385d2bcda23
luked177 <43680876+luked177@users.noreply.github.com>
2023-10-27 15:11:16 +0100

    Update app routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import "./App.css";
 
 function App() {
 	return (
 		<>
 			<p>This is a test for nested routes</p>
+			<Link to={"/path1"}>Path1</Link>
+			<br />
+			<Link to={`/child/${crypto.randomUUID()}`}>Child with useParams</Link>
 			<Outlet />
 		</>
 	);
