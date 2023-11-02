a61bb897074579bbc5d8a5703d25fc9e33af4c51
luked177 <43680876+luked177@users.noreply.github.com>
2023-11-02 16:43:05 +0000

    Update app message

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import { Link, Outlet } from "react-router-dom";
 import "./App.css";
 
 function App() {
-	console.log(import.meta.env.VITE_PATHNAME);
-	console.log(import.meta.env.VITE_TEST);
 	return (
 		<>
-			<p>This is a test for nested routes</p>
+			<p>This is app is deployed to two separate instances using pipelines and env variables.</p>
+			<p>This versions pathname is {import.meta.env.VITE_PATHNAME}</p>
+			<p>This is set through an env variable in the pipeline to deploy!</p>
 			<Link to={"/path1"}>Path1</Link>
 			<br />
 			<Link to={`/child/${crypto.randomUUID()}`}>Child with useParams</Link>
